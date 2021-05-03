e2a3c1b3e32d16185c3dfe0435e952d3397f025e
Amin Khansari <akhansari@users.noreply.github.com>
2021-05-03 14:27:35 +0200

    feat: filters

--- App.fs
+++ App.fs
@@ -8,18 +8,31 @@ open Bolero.Html
 
 type Model =
     { NewTask: string
-      Tasks: Task.Model list }
+      Tasks: Task.Model list
+      Filter: Task.State option
+      FilteredTasks: Map<Task.State, Task.Model list> }
 
 module Model =
+
     let empty =
         { NewTask = String.Empty
-          Tasks = List.empty }
+          Tasks = List.empty
+          Filter = None
+          FilteredTasks = Map.empty.Add(Task.Active, []).Add(Task.Completed, []) }
 
 type Message =
     | SetNew of string
     | Add
     | Remove of Task.Model
+    | SetFilter of Task.State option
     | WrapTask of Task.Model * Task.Message
+
+let private updateTasks model tasks =
+    { model with
+        Tasks = tasks
+        FilteredTasks = Map.empty
+            .Add(Task.Active, tasks |> List.filter (fun t -> t.State = Task.Active))
+            .Add(Task.Completed, tasks |> List.filter (fun t -> t.State = Task.Completed)) }
 
 let update message model =
     match message with
@@ -27,27 +40,31 @@ let update message model =
         { model with NewTask = title }
         , Cmd.none
     | Add ->
-        { model with
-            NewTask = String.Empty
-            Tasks = Task.Model.create model.NewTask :: model.Tasks }
+        Task.Model.addTo model.Tasks model.NewTask
+        |> updateTasks { model with NewTask = String.Empty }
         , Cmd.none
     | Remove task ->
-        { model with Tasks = model.Tasks |> List.filter (fun t -> t.Id <> task.Id) }
+        Task.Model.removeFrom model.Tasks task
+        |> updateTasks model
+        , Cmd.none
+    | SetFilter filter ->
+        { model with Filter = filter }
         , Cmd.none
     | WrapTask (task, taskMsg) ->
         let task, taskCmd = Task.update taskMsg task
-        { model with Tasks = model.Tasks |> List.map (fun t -> if t.Id = task.Id then task else t) }
+        Task.Model.updateIn model.Tasks task
+        |> updateTasks model
         , Cmd.batch [
             Cmd.map WrapTask taskCmd
             if taskMsg = Task.Destroy then Cmd.ofMsg (Remove task)
         ]
 
-let headerView model dispatch =
+let headerView newTask dispatch =
     header [ attr.``class`` "header" ] [
         h1 [] [ text "todos" ]
         input [
             on.keydown (fun e -> if e.Key = "Enter" then dispatch Add)
-            bind.input.string model.NewTask (SetNew >> dispatch)
+            bind.input.string newTask (SetNew >> dispatch)
             attr.``class`` "new-todo"
             attr.placeholder "What needs to be done?"
             attr.autocomplete false
@@ -55,36 +72,57 @@ let headerView model dispatch =
         ]
     ]
 
-let mainView model dispatch =
+let mainView tasks dispatch =
     section [ attr.``class`` "main" ] [
         input [ attr.id "toggle-all"; attr.``class`` "toggle-all"; attr.``type`` "checkbox" ]
         label [ attr.``for`` "toggle-all" ] [ text "Mark all as complete" ]
         ul [ attr.``class`` "todo-list" ] [
-            forEach model.Tasks <| fun task ->
+            forEach tasks <| fun task ->
                 Task.view task (fun m -> WrapTask (task, m) |> dispatch)
         ]
     ]
 
-let footerView model dispatch =
+let footerView remaining filter dispatch =
     footer [ attr.``class`` "footer" ] [
-        span [ attr.``class`` "todo-count" ] []
+        span
+            [ attr.``class`` "todo-count" ]
+            [ strong [] [ text (string remaining) ]
+              text $""" item{if remaining > 1 then "s" else ""} left""" ]
         ul [ attr.``class`` "filters" ] [
-            li [] [ a [] [ text "All" ] ]
-            li [] [ a [] [ text "Active" ] ]
-            li [] [ a [] [ text "Completed" ] ]
+            li [] [ a
+                [ on.click (fun _ -> None |> SetFilter |> dispatch)
+                  attr.classes [ if filter = None then "selected" ] ]
+                [ text "All" ]
+            ]
+            li [] [ a
+                [ on.click (fun _ -> Some Task.Active |> SetFilter |> dispatch)
+                  attr.classes [ if filter = Some Task.Active then "selected" ] ]
+                [ text "Active" ]
+            ]
+            li [] [ a
+                [ on.click (fun _ -> Some Task.Completed |> SetFilter |> dispatch)
+                  attr.classes [ if filter = Some Task.Completed then "selected" ] ]
+                [ text "Completed" ]
+            ]
         ]
         button [ attr.``class`` "clear-completed" ] [ text "Clear completed" ]
     ]
 
 let view model dispatch =
     section [ attr.``class`` "todoapp" ] [
-        headerView model dispatch
+        headerView model.NewTask dispatch
         cond model.Tasks.IsEmpty <| function
         | true -> Empty
         | false ->
+            let tasks =
+                match model.Filter with
+                | Some state -> model.FilteredTasks.[state]
+                | None       -> model.Tasks
+            let remaining =
+                model.FilteredTasks.[Task.Active].Length
             concat [
-                mainView model dispatch
-                footerView model dispatch
+                mainView tasks dispatch
+                footerView remaining model.Filter dispatch
             ]
     ]
 
--- Task.fs
+++ Task.fs
@@ -6,42 +6,53 @@ open Elmish
 open Bolero
 open Bolero.Html
 
+type State = Active | Completed
+
 type Model =
     { Id: Guid
       Title: string
-      Completed: bool }
+      State: State }
 
 module Model =
 
     let empty =
         { Id = Guid.Empty
           Title = String.Empty
-          Completed = false }
+          State = Active }
 
     let create title =
         { Id = Guid.NewGuid ()
           Title = title
-          Completed = false }
+          State = Active }
 
+    let addTo tasks title =
+        create title :: tasks
+
+    let removeFrom tasks task =
+        tasks |> List.filter (fun t -> t.Id <> task.Id)
+
+    let updateIn tasks newTask =
+        tasks |> List.map (fun oldTask -> if oldTask.Id = newTask.Id then newTask else oldTask)
 
 type Message =
-    | Completed of bool
+    | SetCompleted of bool
     | Destroy
 
 let update message model =
     match message with
-    | Completed completed ->
-        { model with Completed = completed }, Cmd.none
+    | SetCompleted completed ->
+        { model with State = if completed then Completed else Active }
+        , Cmd.none
     | Destroy ->
         model, Cmd.none
 
 type Component () =
     inherit ElmishComponent<Model, Message> ()
     override _.View model dispatch =
-        li [ attr.classes [ "todo"; if model.Completed then "completed" ] ] [
+        li [ attr.classes [ "todo"; if model.State = Completed then "completed" ] ] [
             div [ attr.``class`` "view" ] [
                 input
-                    [ bind.``checked`` model.Completed (Completed >> dispatch)
+                    [ bind.``checked`` (model.State = Completed) (SetCompleted >> dispatch)
                       attr.``class`` "toggle"
                       attr.``type`` "checkbox" ]
                 label [] [ text model.Title ]
